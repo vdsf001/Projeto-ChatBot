dad15f8f3051203e7fa12506a1690ff358627424
vdsf001 <vdsf2101@gmail.com>
2020-09-27 18:30:21 -0300

    Reestruturacao do projeto

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from lib import route
 
 app =  Flask(__name__)
+@app.route('/')
+def index():
+    return 'Route is working'
+
+
 if __name__== "main":
     app.run()
 
@@ -10,29 +14,3 @@ if __name__== "main":
 
 
 
-
-
-
-
-
-
-
-
-
-
-# import requests
-
-
-# input_uf  = input('Digite a UF do estado que queira pesquisar: ')
-# request = requests.get('https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/{}'.format(input_uf))
-
-# datas = request.json()
-
-# if 'erro' not in datas:
-#     print('UF DIGITADA ENCONTRADA')
-
-#     print('UF: {}'.format(datas['uf']))
-#     print('ESTADO: {}'.format(datas['state']))  
-#     print('CASOS: {}'.format(datas['cases']))      
-#     print('MORTOS: {}'.format(datas['deaths'])) 
-#     print('SUSPEITAS: {}'.format(datas['suspects']))
