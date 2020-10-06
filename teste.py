142c5d7ed4faa5b770de1dd86dbc59330aff3431
paulo-pf <63054586+paulo-pf@users.noreply.github.com>
2020-10-06 13:29:29 -0300

    Rascunho para testes
    Adição de campos para testar as respostas do Bot

--- teste.py
+++ teste.py
@@ -1,9 +1,20 @@
 from Bot import Bot
 
-lista = [["Edson", "Arão"], ["Vinicius", "Elias"]]
+lista = [["olá", "oi", "tudo bem?"], 
+         ["oi, tudo bem?", "oi, como vai?"],
+         ["consulta de preços","consulta de preços de seguradoras","gostaria de consultar os preços das seguradoras","quero consultar os preços das seguradoras"],
+         ["vou sair","Sair", "quero sair"]]
 
-respostas = ["Achou Arão ou Edson", "Achou Vinicius ou Elias"]
+respostas = ["Olá, como posso ajudar?", 
+             "Estou bem! Gostaria que eu te ajudasse em algo?",
+             "Ok, aguarde só um momento enquanto pesquiso as informações",
+             "Tudo bem. Espero ter ajudado!"]
 
 bot = Bot(lista, respostas)
 
-print(bot.find_word("fdsjfl"))
+pergunta=""
+
+while pergunta!='quero sair':
+    pergunta = input("\nDigite algo:")
+    pergunta=pergunta.lower()
+    print(bot.find_word(pergunta))
