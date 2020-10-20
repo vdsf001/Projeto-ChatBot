f01616f240525abbd81b5bd071880cd3501151bf
Freitas29 <arao.freitas@hotmail.com>
2020-10-20 10:09:29 -0300

    cors config

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify
 from Textos import lista, respostas
 from Bot import Bot
+from flask_cors import CORS, cross_origin
 
 app =  Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/question')
+@cross_origin()
 def index():
     param = request.args.get("pergunta")
     bot = Bot(lista, respostas)
