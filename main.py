2464a33e17d220744a4a54d2f236d1665822cd59
Freitas29 <arao.freitas@hotmail.com>
2020-10-19 23:27:18 -0300

    instância o bot e retorna a resposta

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from Textos import lista, respostas
+from Bot import Bot
 
 app =  Flask(__name__)
 @app.route('/question')
 def index():
     param = request.args.get("pergunta")
+    bot = Bot(lista, respostas)
+
     return jsonify({
-        "resposta": "Testando"
+        "resposta": bot.find_word(param)
     })
 
 if __name__== "main":
