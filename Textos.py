2ac5bcb60259ca1cfb976564928ad1a1e8e507d8
Freitas29 <arao.freitas@hotmail.com>
2020-10-26 22:51:51 -0300

    Mais palavras e ajuste no nome do bot

--- Textos.py
+++ Textos.py
@@ -1,15 +1,16 @@
-Nome='Arão'
+Nome='Baymax'
 
-lista = [["olá", "oi", "tudo bem?"],
+lista = [["olá", "ola", "oi", "tudo bem?"],
          ["oi, tudo bem?", "oi, como vai?"],
-         ["qual o seu nome?", "qual o seu nome", "pode me falar o seu nome?", "pode me falar o seu nome"],
+         ["qual o seu nome?", "qual o seu nome", "como você se chama", "como você se chama?", "nome" "pode me falar o seu nome?", "pode me falar o seu nome"],
          ["consulta", "consultas", "preço", "preco", "preços", "precos", "preços seguradoras", "precos seguradoras", "consulta de preços", "consulta de precos", "consulta de preços de seguradoras", "consulta de precos de seguradoras", "gostaria de consultar os preços das seguradoras", "gostaria de consultar os precos das seguradora", "quero consultar os preços das seguradoras", "quero consultar os precos das seguradoras"],
          ["covid", "coronavirus", "covid-19", "corona", "corona virus"],
          ["casos brasil", "dados brasil", "mortes no brasil", "casos e mortes no brasil", "número de casos e mortes no brasil", "numero de casos e mortes no brasil", "dados covid brasil", "dados coronavirus brasil", "dados covid19 brasil"],
          ["casos mundo", "casos mundiais", "dados mundo", "dados mundiais", "casos e mortes no mundo", "dados covid mundo", "dados coronavirus mundiais", "dados covid19 mundiais",  "dados mundiais do covid", "quero saber os dados mundiais do covid", "gostaria de saber o número de casos do covid no mundo", "gostaria de saber o numero de casos do covid no mundo"],
          ["sintomas", "sintomas do covid", "sintomas coronavirus", "sintomas covid19", "pode me informar os sintomas do covid?", "pode me informar os sintomas do covid", "gostaria de saber os sintomas do covid", "quero saber os sintomas do coronavirus", "quero saber os sintomas do covid19", "quero saber os sintomas do covid"],
          ["prevenção", "prevencao", "prevencao covid", "prevenção covid", "prevencao coronavirus", "prevenção coronavirus", "prevencao covid19", "prevenção covid19", "como posso me prevenir?", "como posso me prevenir"],
-         ["vou sair" , "sair", "quero sair", "obrigado pela ajuda", "obrigada pela ajuda", "agradeço pela ajuda", "estarei saindo agora"]]
+         ["vou sair" , "sair", "quero sair", "obrigado", "obrigado pela ajuda", "obrigada pela ajuda", "agradeço pela ajuda", "estarei saindo agora"],
+         ["O que você pode fazer?", "O que você pode fazer?", "opções", "o que eu posso perguntar?", "o que eu posso perguntar"]]
 
 
 respostas = ["Olá, eu sou " +Nome+ ", como posso ajudar?", 
@@ -21,4 +22,5 @@ respostas = ["Olá, eu sou " +Nome+ ", como posso ajudar?",
              "\nDados Covid Mundo",
              ["Os principais sintomas do Covid-19 incluem:", "-Dificuldade de respirar;","-Febre;","-Tosse.","Se apresentar estes sintomas, por favor, procure ajuda médica para fazer um exame que poderá comprovar se você tem o COVID-19."],
              ["Algumas das principais recomendações para a prevenção do Covid-19 incluem:","-Higienizar bem as mãos com água e sabão ou álcool gel;","-Quando sair de casa, usar uma  máscara que cubra o nariz e a boca;","-Evitar aglomerações;","-Manter uma distância mínima de ao menos 1 metro de outras pessoas."],
-             "Tudo bem. Espero ter sido útil!"]
+             "Tudo bem. Espero ter sido útil!",
+             "Consultar preços\n Sintomas \n Prevenção \n Consultar número de casos"]
